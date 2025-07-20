19d970df7f1e23f451f2b83e3ea0ffddb9e1c223
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-20 16:49:22 +0300

    Initial commit: Add SchoolApp MVC projects with .gitignore setup

---
+++ SchoolApp.WebMvcDbFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolApp.WebMvcDbFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
