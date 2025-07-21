ecfa4110e1b7fbf2abd0fd5964e289761afddcee
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-21 21:22:26 +0300

    feat: add user interface and implementation

---
+++ SchoolApp.WebMvcDbFirst/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SchoolApp.WebMvcDbFirst.Data;
+
+namespace SchoolApp.WebMvcDbFirst.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserAsync(string username, string password);
+        Task<User?> UpdateUserAsync(int id, User user);
+        Task<User?> GetByUsernameAsync(string username);
+        Task<List<User>> GetAllUsersFilteredPaginatedAsync(int pageNumber, int pageSize, List<Func<User, bool>> predicates);
+    }
+}
