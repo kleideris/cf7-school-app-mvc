43863e97da4d66310f926fd8b5134ce45a9acd1a
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-21 19:53:57 +0300

    feat: add unit of work interface and implementation

---
+++ SchoolApp.WebMvcDbFirst/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.WebMvcDbFirst.Repositories
+{
+    public interface IUnitOfWork
+    {
+        // TODO: UserRepository
+        TeacherRepository TeacherRepository { get; }
+        StudentRepository StudentRepository { get; }
+        CourseRepository CourseRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
---
+++ SchoolApp.WebMvcDbFirst/Repositories/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿
+using SchoolApp.WebMvcDbFirst.Data;
+
+namespace SchoolApp.WebMvcDbFirst.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly MvcDbContext _context;
+
+        public UnitOfWork(MvcDbContext context)
+        {
+            _context = context;
+        }
+
+        public TeacherRepository TeacherRepository => new(_context);
+
+        public StudentRepository StudentRepository => new(_context);
+
+        public CourseRepository CourseRepository => new(_context);
+
+        public async Task<bool> SaveAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
