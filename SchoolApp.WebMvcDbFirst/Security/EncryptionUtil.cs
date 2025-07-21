35a8fcf86fa7f8513972abec9d04c719c00e8d45
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-21 21:23:10 +0300

    feat: add bcrypt package and encryption util

---
+++ SchoolApp.WebMvcDbFirst/Security/EncryptionUtil.cs
@@ -0,0 +1,16 @@
+﻿namespace SchoolApp.WebMvcDbFirst.Security
+{
+    public static class EncryptionUtil
+    {
+        public static string Encrypt(string plainText)
+        {
+            var encryptedPassword = BCrypt.Net.BCrypt.HashPassword(plainText);
+            return encryptedPassword;
+        }
+
+        public static bool isValidPassword(string plainText, string cipherText)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainText, cipherText);
+        }
+    }
+}
