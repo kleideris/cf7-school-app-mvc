d02b249d1548801c3b9865bf08c6c1d9d44ca40d
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-22 00:09:10 +0300

    feat: create Teacher Service API

---
+++ SchoolApp.WebMvcDbFirst/Services/ITeacherService.cs
@@ -0,0 +1,14 @@
+﻿using SchoolApp.WebMvcDbFirst.Data;
+using SchoolApp.WebMvcDbFirst.DTO;
+
+namespace SchoolApp.WebMvcDbFirst.Services
+{
+    public interface ITeacherService
+    {
+        Task SignUpUserAsync(TeacherSignUpDTO request);
+        Task<List<User>> GetAllUsersTeachersAsync();
+        Task<List<User>> GetAllUsersTeachersAsync(int pageNumber, int pageSize);
+        Task<int?> GetTeacherCountAsync();
+        Task<User?> GetTeacherByUsernameAsync(string username);
+    }
+}
