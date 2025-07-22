e260f515f25b6e497431429e7be31ea7ec7fadfb
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-22 12:59:24 +0300

    feat: add student service interface and implementation

---
+++ SchoolApp.WebMvcDbFirst/Services/IStudentService.cs
@@ -0,0 +1,13 @@
+﻿using SchoolApp.WebMvcDbFirst.Data;
+
+namespace SchoolApp.WebMvcDbFirst.Services
+{
+    public interface IStudentService
+    {
+        Task<IEnumerable<User>> GetAllStudentsAsync();
+        Task<List<Course>> GetStudentCourseAsync(int id);
+        Task<Student?> GetStudentAsync(int id);
+        Task<bool> DeleteStudentAsync(int id);
+        Task<int> GetStudentCountAsync();
+    }
+}
