f0b3e364dd7bfe418fd0a5f27ec8a90243beb429
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-22 20:46:15 +0300

    feat: add new ModelFirst project

---
+++ SchoolApp.WebMvc.ModelFirst/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolApp.WebMvc.ModeFirst.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
