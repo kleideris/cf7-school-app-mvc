223444ce6009c0cf0ca2e8258877f66252b0413a
Ch. Kleideris <kleiderisch@gmail.com>
2025-07-23 21:35:40 +0300

    feat(WebApi): add Repositories

---
+++ SchoolApp/Repositories/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿namespace SchoolApp.Repositories
+{
+    public interface IUnitOfWork
+    {
+        UserRepository UserRepository { get; }
+        TeacherRepository TeacherRepository { get; }
+        StudentRepository StudentRepository { get; }
+        CourseRepository CourseRepository { get; }
+
+        Task<bool> SaveAsync();
+    }
+}
---
+++ SchoolApp/Repositories/StudentRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolApp.Core.Enums;
+using SchoolApp.Data;
+
+namespace SchoolApp.Repositories
+{
+    public class StudentRepository : BaseRepository<Student>, IStudentRepository
+    {
+        public StudentRepository(SchoolAppDbContext context) 
+            : base(context)
+        {
+        }
+
+        public async Task<Student?> GetByAm(string? am)
+        {
+            return await context.Students
+                .Where(s => s.Am == am)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<List<Course>> GetStudentCoursesAsync(int id)
+        {
+            return await context.Students
+                .Where(s => s.Id == id)
+                .SelectMany(s => s.Courses)
+                .ToListAsync();
+        }
+
+        public async Task<List<User>> GetAllUsersStudentsAsync()
+        {
+            return await context.Users
+                .Where(u => u.UserRole == UserRole.Student)
+                .Include(u => u.Student)
+                .ToListAsync();
+        }
+
+        
+
+        
+    }
+}
